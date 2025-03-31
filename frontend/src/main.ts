f92d473b2751df766b140a3c76311998c2b5938e
Elias Murray <elias.pleijel@gmail.com>
2025-03-31 14:22:18 +0200

    fixade så båda knapparna på index.html funkar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,3 +4,8 @@ const registerButton = document.getElementById("goToRegister");
 registerButton?.addEventListener("click", () => {
   location.href = "/register.html";
 });
+
+const loginButton = document.getElementById("goToLogin");
+loginButton?.addEventListener("click", () => {
+  location.href = "/login.html";
+});
